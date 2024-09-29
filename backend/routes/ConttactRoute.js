f677e8b6f4173167d92006ef9451932aa5544cd5
saiful <azadahmedofficial@gmail.com>
2024-09-29 13:35:40 +0600

    multiple contact route added

--- backend/routes/ConttactRoute.js
+++ backend/routes/ConttactRoute.js
@@ -1,8 +1,25 @@
 const express= require ('express')
 const router= express.Router();
 
+// get all the contacts
 router.route("/").get((req,res) =>{
     res.status(200).json({message:"all the contacts"})
+});
+// creatre contact
+router.route("/").post((req,res) =>{
+    res.status(200).json({message:"Craeate contact"})
+});
+// get single contact
+router.route("/:id").get((req,res) =>{
+    res.status(200).json({message:`Get contact  for ${req.params.id}`})
+});
+// Update single contact
+router.route("/:id").post((req,res) =>{
+    res.status(200).json({message:`Update Contact  for ${req.params.id}`})
+});
+// delete single contact
+router.route("/:id").delete((req,res) =>{
+    res.status(200).json({message:`Delete contact  for ${req.params.id}`})
 });
 
 module.exports= router
