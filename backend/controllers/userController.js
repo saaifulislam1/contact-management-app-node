7641111aece5aae860e843e0584c9ed23a0f4b82
saiful <azadahmedofficial@gmail.com>
2024-10-14 20:01:39 +0600

    User reg crud op done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,35 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 // Register User
 // @acess public
 // route -> POST /api/users/contact
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are necessary");
+  }
+  // check if user already exist or not
+  const userAvailavle = await User.findOne({ email });
+  if (userAvailavle) {
+    res.status(400);
+    throw new Error("Email Already exist");
+  }
+  // password hasing
+  const hasedPassword = await bcrypt.hash(password, 10);
+  const user = User.create({
+    username,
+    email,
+    password: hasedPassword,
+  });
+  console.log(user, "User Created");
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
   res.json({ message: "Register the user" });
 });
 // Login User
